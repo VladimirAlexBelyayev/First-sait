5ed6df665ee290c6ea2cfd479cb701958f796ca1
VladimirAlexBelyayev <137166332+VladimirAlexBelyayev@users.noreply.github.com>
2023-06-20 13:00:29 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+button.addEventListener('click', function() {    
+    output.innerHTML = 'Самые востребованные языки программирования-JavaScript, C#, Python, Go, Ruby'
+});
+
